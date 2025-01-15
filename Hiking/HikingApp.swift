99b64b419696e79c7d0561a4e7adfa46098677be
Nguyen Cong Thu <thunc@vnpay.vn>
2025-01-15 11:45:16 +0700

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Nguyễn Công Thư on 15/1/25.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
